be0d8ac009f4567b25909017da05d2766ea32d6c
Luke Corbett <corbettluke14@gmail.com>
2025-05-22 18:13:05 -0600

    Getting started and ServerFacae plan

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,23 @@
+package client;
+
+import com.google.gson.Gson;
+
+public class ServerFacade {
+    private final String serverUrl;
+    private final Gson gson = new Gson();
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+
+    //make request
+    //register
+    //login
+    //logout
+    //createGame
+    //listGames
+    //joinGame
+    //
+
+}
