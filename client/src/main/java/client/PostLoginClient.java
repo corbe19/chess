87df8f0d5af33513157072da375c38d536636925
Luke Corbett <corbettluke14@gmail.com>
2025-05-26 12:30:34 -0600

    PostLoginREPL/Client help and create implemented

--- client/src/main/java/client/PostLoginClient.java
+++ client/src/main/java/client/PostLoginClient.java
@@ -1,4 +1,35 @@
 package client;
 
+import model.AuthData;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PostLoginClient {
+    private final ServerFacade server;
+    private List<GameData> lastGameList = new ArrayList<>();
+
+    public PostLoginClient(ServerFacade server) {
+        this.server = server;
+    }
+
+    public void help() {
+        System.out.println(" create <NAME> - a game");
+        System.out.println(" list - games");
+        System.out.println(" join <ID> [WHITE|BLACK] - a game");
+        System.out.println(" observe <ID> - a game");
+        System.out.println(" logout - when you are done");
+        System.out.println(" quit - playing chess");
+        System.out.println(" help - with possible commands");
+    }
+
+    public void create(String[] tokens, AuthData auth) throws Exception {
+        if (tokens.length != 2) {
+            throw new IllegalArgumentException("Usage: create <NAME>");
+        }
+        server.createGame(auth, tokens[1]);
+        System.out.println("Game created!");
+    }
+
 }
--- client/src/main/java/repl/PostLoginREPL.java
+++ client/src/main/java/repl/PostLoginREPL.java
@@ -1,4 +1,44 @@
 package repl;
 
+import client.PostLoginClient;
+import model.AuthData;
+
+import java.util.Scanner;
+
 public class PostLoginREPL {
+    private final Scanner scanner;
+    private final PostLoginClient client;
+
+    public PostLoginREPL(Scanner scanner, client.ServerFacade server) {
+        this.scanner = scanner;
+        this.client = new PostLoginClient(server);
+    }
+
+    public AuthData run(AuthData auth) {
+        while (true) {
+            System.out.print("[LOGGED_IN] >>> ");
+            String[] tokens = scanner.nextLine().trim().split("\\s+");
+
+            try {
+                return switch (tokens[0].toLowerCase()) {
+                    case "help" -> {
+                        client.help();
+                        yield auth;
+                    }
+                    case "create" -> {
+                        client.create(tokens, auth);
+                        yield auth;
+                    }
+                    default -> {
+                        System.out.println("Unkown command: Type 'help' for options.");
+                        yield auth;
+                    }
+                };
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+
 }
